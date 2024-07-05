5f0f1445d053ec53b3bae9c462b539896e34c485
luciene.oliveira <luciene.oliveira@VAR223>
2024-07-05 19:17:15 -0300

    Reajuste dos menus

--- frmModulos.vb
+++ frmModulos.vb
@@ -2,6 +2,8 @@
 Imports Padaria
 Imports ProjetoBanco
 Imports SistemaVendas
+Imports Escola
+Imports ProjetoLocadora
 Public Class frmModulos
     Private Sub btnOficina_Click(sender As Object, e As EventArgs) Handles btnOficina.Click
         Dim janela As New OficinaMecanica.frmPrincipal()
@@ -16,11 +18,6 @@ Public Class frmModulos
     Private Sub btnLocadora_Click(sender As Object, e As EventArgs) Handles btnLocadora.Click
         Dim janela As New ProjetoLocadora.frmPrincipal()
         janela.Show()
-    End Sub
-
-    Private Sub btnEscola_Click(sender As Object, e As EventArgs) Handles btnEscola.Click
-        'Dim janela As New Escola.frmPrincipal()
-        'janela.Show()
     End Sub
 
     Private Sub btnBanco_Click(sender As Object, e As EventArgs) Handles btnBanco.Click
